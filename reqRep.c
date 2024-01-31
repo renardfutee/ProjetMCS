d15a7b503ce092be9b9702af1ea8a7d4c25eecde
renardfutee <mathis.rade@gmail.com>
2024-01-31 17:13:52 +0100

    Menu

--- reqRep.c
+++ reqRep.c
@@ -8,7 +8,12 @@ int connect(char *pseudo)
 {
 }
 
-
+/********************************************************************************************************************************************************************************************************
+	fonction	:	int connect(char *pseudo); 
+	brief		: 	Connecte ou crée l'utilisateur depuis son pseudo 
+	param		: 	pseudo = pseudo de connection de l'utilisateur 
+	result		: 	retourn 1 si un nouel utilisateur a été créé, 0 si l'utilisateur existait et -1 en cas d'erreur 
+********************************************************************************************************************************************************************************************************/
 void affichageJoueur(){
-
+    
 }
