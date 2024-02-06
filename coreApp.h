3e1c61af03d6ca06ca5bf6e44093dac221b7a62d
Achrafe09 <achrafe0903@gmail.com>
2024-02-06 22:22:49 +0100

    Ca fonctionne

--- coreApp.h
+++ coreApp.h
@@ -32,11 +32,22 @@ char* findRandomTheme();
  * 
  * @param connected_pseudo 
  * @param recherchePseudo 
+ * @return int
  */
 int creategame(const char* connected_pseudo, const char* recherchePseudo);
 
 /**
  * @brief 
  * 
+ * @return char* 
  */
 char** fetchAllPlayers(); 
+
+/**
+ * @brief 
+ * 
+ * @param id_match 
+ * @param id_manche 
+ * @return char* 
+ */
+char *RecupTheme(int id_match, int id_manche)
